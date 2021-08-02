dc6f11680a117b2fb649523f1d479b3fdc9ce9d2
Morgan C <mjcro10@student.monash.edu>
2021-08-02 12:59:25 +1000

    Updated README with installation instructions

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# osx-setup-config
+# New OSX Setup
+Setup a new OSX with my favourite settings and applications.
+
+# Installation
+1. Git clone this repository somewhere on your machine
+2. Go to this directory and run ```./osx-setup-command.sh```
+3. Answer a couple questions and wait for everything to download! 
+
+
+# Post Installation Instructions
+- Turning off cmd + space for mac search
+- Adding sidebare to be really small and on the side
+- Idk, whatever else I run into while doing my initial setup
+
+# TODO
+- Add custom .zshrc file
+- Install custom .zshrc file
+- Have so every tenth terminal that opens has a quote from cowsay and fortune
+- Update vscode settings and extensions
+- Setup CMD+Space to launch alfred if possible by code
+- Update iterm2 settings -> colors, keep directory open new shell, keyboard shortcuts
--- osx-setup-command.sh
+++ osx-setup-command.sh
@@ -22,46 +22,41 @@ fi
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew install git
-# set CMD+space to launch alfred
-brew install --cask alfred
-brew install --cask cheatsheet
-brew install --cask grammarly
-brew install --cask rectangle
+brew install java
+brew install --cask \
+    alfred \
+    cheatsheet \
+    grammarly \
+    rectangle
 
 ### --- Terminal Customisation ---
-# Update iterm2 settings -> colors, keep directory open new shell, keyboard shortcuts
 brew install --cask iterm2
-brew install autojump
-# Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-brew install cowsay 
-brew install fortune
-brew install awscli
-# TODO: Every 10th open terminal, cowsay says a fortune 
-# TODO: Add ability to download custom .zshrc file
+brew install \
+    cowsay \ 
+    fortune
+    autojump \
+    awscli
 
 ### --- Applications ---
-brew install --cask firefox
-brew install --cask google-chrome
-brew install --cask tor-browser
-brew install --cask docker
-brew install --cask dash
-brew install --cask intellij-idea
-brew install --cask pycharm
-brew install --cask postman
-brew install --cask spotify
-brew install --cask vlc
-brew install --cask discord
-brew install --cask whatsapp
-brew install --cask appcleaner
-brew install --cask bitwarden
-brew install --cask fork
-# Ability to download from mac app store throught CLI
-brew install mas-cli/tap/mas
-mas lucky trello
-# TODO: Update vscode settings
-# TODO: Install vscode extensions 
-brew install --cask visual-studio-code
+brew install --cask \
+    firefox \
+    google-chrome \
+    tor-browser \
+    docker \
+    dash \
+    intellij-idea \
+    pycharm \
+    postman \
+    spotify \
+    vlc \
+    discord \
+    whatsapp \
+    appcleaner \
+    bitwarden \
+    fork \
+    visual-studio-code
+
 if "$download_vmware"; then
     brew install --cask vmware-fusion
 fi
@@ -69,5 +64,13 @@ if "$download_torrent"; then
     brew install --cask qbittorrent
 fi
 
+# Ability to download from mac app store throught CLI
+brew install mas-cli/tap/mas
+mas lucky trello
+
 ### --- MISC ---
+# Use this folder for all new projects
 mkdir ~/dev
+
+# Delete this directory once setup is complete
+rm -rf -- "$(pwd -P)" && cd ..
